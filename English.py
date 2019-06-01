330b730b7d065151f7efc8478bf363069fe10e8c
adi19v <45130202+adi19v@users.noreply.github.com>
2019-06-02 02:21:29 +0530

    Update English.py

--- English.py
+++ English.py
@@ -833,8 +833,8 @@ Available commands are:
 
 "Federations_help": """Ah, group management. It's all fun and games, until you start getting spammers in, and you need to ban them. Then you need to start banning more, and more, and it gets painful.
 But then you have multiple groups, and you don't want these spammers in any of your groups - how can you deal? Do you have to ban them manually, in all your groups?
-Inspired by [Rose bot](t.me/MissRose_bot)
-No more! With federations, you can make a ban in one chat overlap to all your other chats.
+
+/n No more! With federations, you can make a ban in one chat overlap to all your other chats.
 You can even appoint federation admins, so that your trustworthy admins can ban across all the chats that you want to protect.
 Commands:
  - /newfed <fedname>: creates a new federation with the given name. Users are only allowed to own one federation. This method can also be used to change the federation name. (max 64 characters)
